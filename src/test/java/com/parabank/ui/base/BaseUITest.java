e762581f3392dd177da467a54819b343f2dc1eb8
Alona Kunyk <alona.kunyk.seba@gmail.com>
2026-06-26 19:50:32 +0200

    refactoring: add After Method to BaseUITest class, that save screenshots

--- src/test/java/com/parabank/ui/base/BaseUITest.java
+++ src/test/java/com/parabank/ui/base/BaseUITest.java
@@ -3,8 +3,11 @@ package com.parabank.ui.base;
 import com.microsoft.playwright.Page;
 import com.parabank.setup.PlaywrightFactory;
 import com.parabank.utils.ConfigurationManager;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.nio.file.Paths;
 
 public class BaseUITest {
     protected Page page;
@@ -15,7 +18,12 @@ public class BaseUITest {
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            page.screenshot(new Page.ScreenshotOptions()
+                    .setPath(Paths.get("target/screenshots/" + result.getName() + ".png"))
+                    .setFullPage(true));
+        }
         PlaywrightFactory.removeThreadLocals();
     }
 }
--- src/test/java/com/parabank/ui/base/BaseUITestWithRegistration.java
+++ src/test/java/com/parabank/ui/base/BaseUITestWithRegistration.java
@@ -3,7 +3,9 @@ package com.parabank.ui.base;
 import com.parabank.models.ui.User;
 import com.parabank.pages.LoginPage;
 import com.parabank.pages.MainPage;
+import com.parabank.setup.PlaywrightFactory;
 import com.parabank.utils.DataGenerator;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
